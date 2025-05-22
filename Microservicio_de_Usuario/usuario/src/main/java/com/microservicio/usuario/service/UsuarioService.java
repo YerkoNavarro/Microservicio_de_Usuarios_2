2941b108acf5f9541485a43c2f05687c6c889dfe
YerkoNavarro <yerkonavarro7@gmail.com>
2025-05-21 22:56:37 -0400

    metodo agregar y traer Usuario

--- Microservicio_de_Usuario/usuario/src/main/java/com/microservicio/usuario/service/UsuarioService.java
+++ Microservicio_de_Usuario/usuario/src/main/java/com/microservicio/usuario/service/UsuarioService.java
@@ -3,6 +3,8 @@ package com.microservicio.usuario.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.microservicio.usuario.entity.UsuarioEntity;
+import com.microservicio.usuario.model.Usuario;
 import com.microservicio.usuario.repository.UsuarioRepository;
 
 @Service
@@ -10,6 +12,51 @@ public class UsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
-    
+
+    UsuarioEntity ue = new UsuarioEntity();
+
+    public String agregarUsuario(Usuario u){
+
+        
+        if(usuarioRepository.existsByIdUsuario(u.getIdUsuario())){
+            try {
+                ue.setIdUsuario(u.getIdUsuario());
+                ue.setNombre(u.getNombre());
+                ue.setCorreo(u.getCorreo());
+                ue.setContrasena(u.getContrasena());
+                usuarioRepository.save(ue);
+                return "usuario guuardado correctamente"; 
+            } catch (Exception e) {
+                return "error al guardar el usuario"+ e.getMessage();
+            }
+            
+        }else{
+            return "el usuario ya existe";
+        }
+        
+
+
+    }
+    public Usuario traerUsuario(int id){
+
+        try {
+            UsuarioEntity usuarioDB = usuarioRepository.findByIdUsuario(id);
+            if (usuarioDB != null) {
+                Usuario u = new Usuario();
+                u.setIdUsuario(u.getIdUsuario());
+                u.setNombre(usuarioDB.getNombre());
+                u.setCorreo(usuarioDB.getCorreo());
+                u.setContrasena(usuarioDB.getContrasena());
+                return u;
+            } 
+
+
+        } catch (Exception e) {
+            
+            System.out.println(e.getMessage());
+        }
+        return null;
+
+    }
 
 }
