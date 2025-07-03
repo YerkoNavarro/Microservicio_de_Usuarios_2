33b986e1696bc1af5b09aac267ec0244fed69973
YerkoNavarro <yerkonavarro7@gmail.com>
2025-07-03 00:14:58 -0400

    desp 6

--- src/main/java/com/microservicio/usuario/model/Usuario.java
+++ src/main/java/com/microservicio/usuario/model/Usuario.java
@@ -1,5 +1,6 @@
 package com.microservicio.usuario.model;
 
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Usuario {
 
+
+    @Id
     private int idUsuario;
+    
     private String nombre;
     private String correo;
     private String contrasena;
